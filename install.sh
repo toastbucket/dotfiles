98f8ce6ff18ae26ca386b498ee57ac10e45d8f91
Nick Elsmore <nicholaselsmore@gmail.com>
2019-11-04 13:53:51 -0800

    install: add packages and fix typo

--- install.sh
+++ install.sh
@@ -7,16 +7,25 @@ set -e
 ##
 pacman_packages=(
     android-tools minicom openocd ctags
-    ripgrep fd docker openssl wget curl 
-    git rofi vim cmake ctags tig 
+    ripgrep fd docker openssl wget curl
+    git rofi vim cmake ctags tig zathura
+    cargo
 )
 
 pacman_args="--needed --noconfirm"
 
 sudo pacman -Syyu ${pacman_args}
-sudo pacman -Sy ${pacman_args} ${pacman_all[*]}
+sudo pacman -Sy ${pacman_args} ${pacman_packages[*]}
 
 ##
 # Install dotfiles
 ##
 rsync -avr --exclude '.git' --exclude 'install.sh' . ~
+
+##
+# Configure system
+##
+sudo systemctl enable docker
+
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
