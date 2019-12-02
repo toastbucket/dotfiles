c307560f6e5d790b445be91a7408eee0bf7d2d33
Nick Elsmore <nicholaselsmore@gmail.com>
2019-12-02 12:21:08 -0800

    install: only perform actions if necessary

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -e
 
 ##
-# Install packages
+# Install and remove packages
 ##
 pacman_packages=(
     android-tools minicom openocd ctags
@@ -20,9 +20,19 @@ pacman_args="--needed --noconfirm"
 
 sudo pacman -Syyu ${pacman_args}
 sudo pacman -Sy ${pacman_args} ${pacman_packages[*]}
-sudo pacman -Rcn --noconfirm ${pacman_uninstall[*]}
 
-curl https://sh.rustup.rs -sSf | sh
+for i in ${pacman_uninstall[@]}; do
+	if [ ! sudo pacman -Q $i 2> /dev/null ]; then
+		echo "$i installed, removing it"
+		sudo pacman -Rcn --noconfirm $i
+	fi
+done
+
+if [ ! -d ~/.cargo ]; then
+	curl https://sh.rustup.rs -sSf | sh
+fi
+
+rustup update
 
 ##
 # Install dotfiles
@@ -33,9 +43,17 @@ rsync -avr --exclude '.git' --exclude 'install.sh' . ~
 # Configure system
 ##
 sudo systemctl enable docker
+sudo systemctl start docker
 
-sudo groupadd docker
-sudo groupadd dialout
+if ! grep -q docker /etc/group; then
+	echo "docker group not found, adding it"
+	sudo groupadd docker
+	sudo usermod -aG docker ${USER}
+fi
 
-sudo usermod -aG docker ${USER}
-sudo usermod -aG dialout ${USER}
+if ! grep -q dialout /etc/group; then
+	echo "dialout group not found, adding it"
+	sudo groupadd dialout
+	sudo usermod -aG dialout ${USER}
+fi
+
