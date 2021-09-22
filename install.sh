d216c2031d15cb5df71f15298d24137476ab8ba0
Nick Elsmore <nelsmore@witekio.com>
2021-09-22 10:17:03 -0700

    install: refactor script

--- install.sh
+++ install.sh
@@ -2,14 +2,11 @@
 
 set -e
 
-##
-# Install and remove packages
-##
 pacman_packages=(
     android-tools android-udev minicom
     openocd ctags ripgrep fd docker openssl
     wget curl git rofi gvim cmake ctags tig
-    zathura udevil gnome-calculator
+    zathura udevil gnome-calculator zsh
     unzip autorandr
 )
 
@@ -19,42 +16,59 @@ pacman_uninstall=(
 
 pacman_args="--needed --noconfirm"
 
-sudo pacman -Syyu ${pacman_args}
-sudo pacman -Sy ${pacman_args} ${pacman_packages[*]}
+install_system_packages() {
+    sudo pacman -Syyu ${pacman_args}
 
-for i in ${pacman_uninstall[@]}; do
-	if [ ! sudo pacman -Q $i 2> /dev/null ]; then
-		echo "$i installed, removing it"
-		sudo pacman -Rcn --noconfirm $i
-	fi
-done
+    for i in ${pacman_uninstall[@]}; do
+            if [ ! sudo pacman -Q $i 2> /dev/null ]; then
+                    echo "$i installed, removing it"
+                    sudo pacman -Rcn --noconfirm $i
+            fi
+    done
 
-if [ ! -d ~/.cargo ]; then
-	curl https://sh.rustup.rs -sSf | sh
-fi
+    sudo pacman -Sy ${pacman_args} ${pacman_packages[*]}
 
-rustup update
 
-##
-# Install dotfiles
-##
-rsync -avr --exclude '.git' --exclude 'install.sh' . ~
+    if [ ! -d ~/.cargo ]; then
+            curl https://sh.rustup.rs -sSf | sh
+    fi
 
-##
-# Configure system
-##
-sudo systemctl enable docker
-sudo systemctl start docker
+    source $HOME/.cargo/env
+    rustup update
 
-if ! grep -q docker /etc/group; then
-	echo "docker group not found, adding it"
-	sudo groupadd docker
-	sudo usermod -aG docker ${USER}
-fi
+    touch installed
+}
 
-if ! grep -q dialout /etc/group; then
-	echo "dialout group not found, adding it"
-	sudo groupadd dialout
-	sudo usermod -aG dialout ${USER}
-fi
+configure_shell() {
+    if [ ! -d ~/.oh-my-zsh ]; then
+        sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    fi
+    sudo usermod -s /bin/zsh ${USER}
+}
+
+install_dotfiles() {
+    rsync -avr --exclude '.git' --exclude 'install.sh' . ~
+}
+
+configure_system() {
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    
+    if ! grep -q docker /etc/group; then
+        echo "docker group not found, adding it"
+        sudo groupadd docker
+        sudo usermod -aG docker ${USER}
+    fi
+    
+    if ! grep -q dialout /etc/group; then
+        echo "dialout group not found, adding it"
+        sudo groupadd dialout
+        sudo usermod -aG dialout ${USER}
+    fi
+}
+
+install_system_packages
+configure_shell
+install_dotfiles
+configure_system
 
