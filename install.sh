ffb20725500c3f975efc26d44b906faddace6da6
Nick Elsmore <nelsmore@witekio.com>
2021-09-24 10:22:47 -0700

    switch to alacritty

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ pacman_packages=(
     wget curl git rofi gvim cmake ctags tig
     zathura udevil gnome-calculator zsh
     unzip autorandr libreoffice diff-so-fancy
-    repo
+    repo alacritty
 )
 
 pacman_uninstall=(
