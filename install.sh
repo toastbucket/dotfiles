0702bc8162683daea571b403d5be82d05c053c3e
Nick Elsmore <nelsmore@witekio.com>
2022-07-01 11:18:34 -0700

    i3: add screenshot binding

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ pacman_packages=(
     wget curl git rofi gvim cmake ctags tig
     zathura udevil gnome-calculator zsh
     unzip autorandr libreoffice diff-so-fancy
-    repo alacritty pulseaudio xclip
+    repo alacritty pulseaudio xclip flameshot
 )
 
 pacman_uninstall=(
